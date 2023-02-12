c3688fd968b4310dc49c9f65ddcf8baf8301b47a
Wink Saville <wink@saville.com>
2023-02-12 11:01:31 -0800

    refactor: Move the thread::spawn to `fn do_it`
    Separating concerns.

--- send_receiver/README.md
+++ send_receiver/README.md
@@ -1,20 +1,43 @@
 # Experiment with sending a receiver
 
 Verify it's possible to send a Receiver to another thread via a crossbeam_channel.
-Not only does it work, but nicely you can do it via a `BoxMsgAny` type!
+Not only does it work, but you can do it via a `BoxMsgAny` type!
 
 ## Run:
 
 ```
-wink@3900x 23-02-12T15:58:03.651Z:~/prgs/rust/myrepos/exper_crossbeam_channel/send_receiver (main)
-$ cargo run
-   Compiling send_receiver v0.1.0 (/home/wink/prgs/rust/myrepos/exper_crossbeam_channel/send_receiver)
-    Finished dev [unoptimized + debuginfo] target(s) in 0.23s
-     Running `/home/wink/prgs/rust/myrepos/exper_crossbeam_channel/target/debug/send_receiver`
-thread:+
-thread: GOT a msg_receiver=MsgReceiver { receiver: Receiver { .. } }
-thread: from msg_receiver.recv() GOT a msg_done=MsgDone
-thread:-
+wink@3900x 23-02-12T19:01:58.779Z:~/prgs/rust/myrepos/exper_crossbeam_channel/send_receiver (main)
+$ cargo run --release
+    Finished release [optimized] target(s) in 0.01s
+     Running `/home/wink/prgs/rust/myrepos/exper_crossbeam_channel/target/release/send_receiver`
+main:+
+main: sending MsgReceiver { name: "rx1", receiver: Receiver { .. } } on wait_for_msg_receiver_tx
+thread1:+
+thread1: waiting for receiver, will panic if error receiving
+thread1: GOT initial receiver=MsgReceiver { name: "rx1", receiver: Receiver { .. } }
+thread1: waiting on rx1
+main: sending MsgHello { greeting: "Hello, via tx1" } on tx1
+main: sending () on tx1
+thread1: GOT on rx1 MsgHello { greeting: "Hello, via tx1" }
+thread1: waiting on rx1
+thread1: GOT on rx1 ()
+thread1: waiting on rx1
+main: sending 1 on tx1
+main: sending Receiver { .. } on tx1
+thread1: GOT on rx1 1
+thread1: waiting on rx1
+thread1: GOT on rx1 Receiver { .. }
+thread1: waiting on rx1
+main: sending MsgReceiver { name: "rx2", receiver: Receiver { .. } } on tx1
+main: sending MsgHello { greeting: "Hello, via tx2" } on tx2
+thread1: GOT on rx1 MsgReceiver { name: "rx2", receiver: Receiver { .. } }, switching receivers
+thread1: waiting on rx2
+main: sending MsgDone on tx2, thread should stop
+thread1: GOT on rx2 MsgHello { greeting: "Hello, via tx2" }
+thread1: waiting on rx2
+thread1: GOT on rx2 MsgDone, STOPPING thread
+thread1:-
+main:+
 ```
 
 ## License
--- send_receiver/src/main.rs
+++ send_receiver/src/main.rs
@@ -1,4 +1,4 @@
-use std::{any::Any, thread};
+use std::{any::Any, thread::{self, JoinHandle}};
 
 use crossbeam_channel::{unbounded, Receiver, bounded};
 
@@ -17,49 +17,53 @@ struct MsgHello { pub greeting: String }
 #[derive(Debug, Clone)]
 struct MsgDone {}
 
+fn do_it(name: String, wait_for_rx: Receiver<MsgReceiver>) -> JoinHandle<()> {
+    thread::spawn(move || {
+        println!("{name}:+");
+
+        println!("{name}: waiting for receiver, will panic if error receiving");
+        let mut receiver = wait_for_rx.recv().unwrap();
+        println!("{name}: GOT initial receiver={receiver:?}");
+
+        loop {
+            println!("{name}: waiting on {}", receiver.name);
+            let msg = receiver.receiver.recv().unwrap();
+            if let Some(msg_received) = msg.downcast_ref::<MsgHello>() {
+                println!("{name}: GOT on {} {msg_received:?}", receiver.name);
+            } else if let Some(msg_receiver) = msg.downcast_ref::<Receiver<BoxMsgAny>>() {
+                println!("{name}: GOT on {} {msg_receiver:?}", receiver.name);
+            } else if let Some(msg_received) = msg.downcast_ref::<i32>() {
+                println!("{name}: GOT on {} {msg_received:?}", receiver.name);
+            } else if let Some(msg_received) = msg.downcast_ref::<()>() {
+                println!("{name}: GOT on {} {msg_received:?}", receiver.name);
+            } else if let Some(msg_received) = msg.downcast_ref::<MsgDone>() {
+                println!("{name}: GOT on {} {msg_received:?}, STOPPING thread", receiver.name);
+                break;
+            } else if let Some(msg_received) = msg.downcast_ref::<MsgReceiver>() {
+                println!("{name}: GOT on {} {msg_received:?}, switching receivers", receiver.name);
+                receiver = msg_received.clone();
+            } else {
+                panic!("{name}: GOT on {} unknown msg", receiver.name);
+            }
+        }
+        println!("{name}:-");
+    })
+}
+
 fn main() {
+    println!("main:+");
     let (wait_for_receiver_tx, wait_for_receiver_rx) = bounded::<MsgReceiver>(0);
     let (tx1, rx1) = unbounded::<BoxMsgAny>();
     let (tx2, rx2) = unbounded::<BoxMsgAny>();
 
-    let thread1_wait_for_rx = wait_for_receiver_rx.clone();
+    let thread1_handle = do_it("thread1".to_string(), wait_for_receiver_rx.clone());
 
-    // Spawn a new thread to send messages to the channels
-    let thread1_handle = thread::spawn(move || {
-        println!("thread1:+");
-
-        println!("thread1: waiting for receiver, will panic if not received");
-        let mut receiver = thread1_wait_for_rx.recv().unwrap();
-        println!("thread1: GOT initial receiver={receiver:?}");
-
-        loop {
-            println!("thread1: waiting on {}", receiver.name);
-            let msg = receiver.receiver.recv().unwrap();
-            if let Some(msg_received) = msg.downcast_ref::<MsgHello>() {
-                println!("thread1: GOT on {} {msg_received:?}", receiver.name);
-            } else if let Some(msg_receiver) = msg.downcast_ref::<Receiver<BoxMsgAny>>() {
-                println!("thread1: GOT on {} {msg_receiver:?}", receiver.name);
-            } else if let Some(msg_received) = msg.downcast_ref::<i32>() {
-                println!("thread1: GOT on {} {msg_received:?}", receiver.name);
-            } else if let Some(msg_received) = msg.downcast_ref::<()>() {
-                println!("thread1: GOT on {} {msg_received:?}", receiver.name);
-            } else if let Some(msg_received) = msg.downcast_ref::<MsgDone>() {
-                println!("thread1: GOT on {} {msg_received:?}, STOPPING thread", receiver.name);
-                break;
-            } else if let Some(msg_received) = msg.downcast_ref::<MsgReceiver>() {
-                println!("thread1: GOT on {} {msg_received:?}, switching receivers", receiver.name);
-                receiver = msg_received.clone();
-            } else {
-                panic!("thread1: GOT on {} unknown msg", receiver.name);
-            }
-        }
-        println!("thread1:-");
-    });
+    // Set initial MsgReceiver rx1
+    let msg = MsgReceiver { name: "rx1".to_string(), receiver: rx1.clone()};
+    println!("main: sending {msg:?} on wait_for_msg_receiver_tx");
+    wait_for_receiver_tx.send(msg).unwrap();
 
     // NOTE: All of these sends will maybe sent before the thread even starts!
-
-    // Set initial receiver
-    wait_for_receiver_tx.send(MsgReceiver { name: "rx1".to_string(), receiver: rx1.clone()}).unwrap();
 
     // Send some messages that are NOT MsgReceiver
     let msg = Box::new(MsgHello { greeting: "Hello, via tx1".to_string() });
@@ -75,7 +79,7 @@ fn main() {
     println!("main: sending {msg:?} on tx1");
     tx1.send(msg).unwrap();
 
-    // Send new MsgReceiver
+    // Send new MsgReceiver rx2
     let msg = Box::new(MsgReceiver { name: "rx2".to_string(), receiver: rx2.clone() });
     println!("main: sending {msg:?} on tx1");
     tx1.send(msg).unwrap();
@@ -88,4 +92,5 @@ fn main() {
     tx2.send(msg).unwrap();
 
     thread1_handle.join().unwrap();
+    println!("main:+");
 }
