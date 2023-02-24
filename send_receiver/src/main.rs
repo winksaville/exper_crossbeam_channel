7fb0f1fbf683ace288ad9d2e5774197fbd3b03a1
Wink Saville <wink@saville.com>
2023-02-23 16:19:05 -0800

    refactor: reformated with `cargo fmt`

--- send_receiver/src/main.rs
+++ send_receiver/src/main.rs
@@ -1,6 +1,9 @@
-use std::{any::Any, thread::{self, JoinHandle}};
+use std::{
+    any::Any,
+    thread::{self, JoinHandle},
+};
 
-use crossbeam_channel::{unbounded, Receiver, bounded};
+use crossbeam_channel::{bounded, unbounded, Receiver};
 
 type BoxMsgAny = Box<dyn Any + Send>;
 
@@ -12,7 +15,9 @@ struct MsgReceiver {
 
 #[allow(unused)]
 #[derive(Debug, Clone)]
-struct MsgHello { pub greeting: String }
+struct MsgHello {
+    pub greeting: String,
+}
 
 #[derive(Debug, Clone)]
 struct MsgDone {}
@@ -37,10 +42,16 @@ fn do_it(name: String, wait_for_rx: Receiver<MsgReceiver>) -> JoinHandle<()> {
             } else if let Some(msg_received) = msg.downcast_ref::<()>() {
                 println!("{name}: GOT on {} {msg_received:?}", receiver.name);
             } else if let Some(msg_received) = msg.downcast_ref::<MsgDone>() {
-                println!("{name}: GOT on {} {msg_received:?}, STOPPING thread", receiver.name);
+                println!(
+                    "{name}: GOT on {} {msg_received:?}, STOPPING thread",
+                    receiver.name
+                );
                 break;
             } else if let Some(msg_received) = msg.downcast_ref::<MsgReceiver>() {
-                println!("{name}: GOT on {} {msg_received:?}, switching receivers", receiver.name);
+                println!(
+                    "{name}: GOT on {} {msg_received:?}, switching receivers",
+                    receiver.name
+                );
                 receiver = msg_received.clone();
             } else {
                 panic!("{name}: GOT on {} unknown msg", receiver.name);
@@ -56,17 +67,22 @@ fn main() {
     let (tx1, rx1) = unbounded::<BoxMsgAny>();
     let (tx2, rx2) = unbounded::<BoxMsgAny>();
 
-    let thread1_handle = do_it("thread1".to_string(), wait_for_receiver_rx.clone());
+    let thread1_handle = do_it("thread1".to_string(), wait_for_receiver_rx);
 
     // Set initial MsgReceiver rx1
-    let msg = MsgReceiver { name: "rx1".to_string(), receiver: rx1.clone()};
+    let msg = MsgReceiver {
+        name: "rx1".to_string(),
+        receiver: rx1.clone(),
+    };
     println!("main: sending {msg:?} on wait_for_msg_receiver_tx");
     wait_for_receiver_tx.send(msg).unwrap();
 
     // NOTE: All of these sends will maybe sent before the thread even starts!
 
     // Send some messages that are NOT MsgReceiver
-    let msg = Box::new(MsgHello { greeting: "Hello, via tx1".to_string() });
+    let msg = Box::new(MsgHello {
+        greeting: "Hello, via tx1".to_string(),
+    });
     println!("main: sending {msg:?} on tx1");
     tx1.send(msg).unwrap();
     let msg = Box::new(());
@@ -80,11 +96,16 @@ fn main() {
     tx1.send(msg).unwrap();
 
     // Send new MsgReceiver rx2
-    let msg = Box::new(MsgReceiver { name: "rx2".to_string(), receiver: rx2.clone() });
+    let msg = Box::new(MsgReceiver {
+        name: "rx2".to_string(),
+        receiver: rx2.clone(),
+    });
     println!("main: sending {msg:?} on tx1");
     tx1.send(msg).unwrap();
 
-    let msg = Box::new(MsgHello { greeting: "Hello, via tx2".to_string() });
+    let msg = Box::new(MsgHello {
+        greeting: "Hello, via tx2".to_string(),
+    });
     println!("main: sending {msg:?} on tx2");
     tx2.send(msg).unwrap();
     let msg = Box::new(MsgDone {});
